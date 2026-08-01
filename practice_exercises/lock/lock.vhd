c387516bb4358826e03e539ec4df7e937b58d2a5
Roi Lopez Barata <137203489+RoiCorporation@users.noreply.github.com>
2026-08-01 13:29:17 +0200

    Implemented a lock fsm.

---
+++ practice_exercises/lock/lock.vhd
@@ -0,0 +1,95 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity lock is
+
+    -- Constants
+    generic (
+        combination_width : integer := 8;
+        retries           : integer := 3;
+        cycles_door_open  : integer := 5
+    );
+
+    -- Ports
+    port (
+        rst                   : in std_logic;
+        clk                   : in std_logic;
+        edit_lock_combination : in std_logic;
+        check_combination     : in std_logic;
+        input_combination     : in std_logic_vector(combination_width - 1 downto 0);
+        door_open             : out std_logic := '1'
+    );
+end entity lock;
+
+architecture rtl of lock is
+    type lock_state is (
+        EDIT_COMBINATION,
+        DOOR_LOCKED,
+        DOOR_UNLOCKED,
+        DOOR_PERMANENTLY_LOCKED
+    );
+    signal state                     : lock_state;
+    signal correct_combination       : std_logic_vector(combination_width - 1 downto 0) := (others => '0');
+    signal retries_left              : integer                                          := retries;
+    signal cycles_door_unlocked_left : integer                                          := cycles_door_open;
+
+begin
+
+    p_beh : process (clk)
+    begin
+
+        if rising_edge(clk) then
+            if rst = '1' then
+                state                     <= EDIT_COMBINATION;
+                door_open                 <= '1';
+                retries_left              <= retries;
+                cycles_door_unlocked_left <= cycles_door_open;
+
+            elsif rst = '0' then
+                case state is
+                    when EDIT_COMBINATION =>
+                        if edit_lock_combination = '1' then
+                            correct_combination <= input_combination;
+                        elsif edit_lock_combination = '0' then
+                            state     <= DOOR_LOCKED;
+                            door_open <= '0';
+                        end if;
+
+                    when DOOR_LOCKED =>
+                        if retries_left = 0 then
+                            state <= DOOR_PERMANENTLY_LOCKED;
+                        else
+                            if check_combination = '1' then
+                                if input_combination = correct_combination then
+                                    state        <= DOOR_UNLOCKED;
+                                    door_open    <= '1';
+                                    retries_left <= retries;
+                                else
+                                    door_open    <= '0';
+                                    retries_left <= retries_left - 1;
+                                end if;
+                            end if;
+                        end if;
+
+                    when DOOR_UNLOCKED =>
+                        if edit_lock_combination = '1' then
+                            state <= EDIT_COMBINATION;
+                        else
+                            if cycles_door_unlocked_left = 0 then
+                                state                     <= DOOR_LOCKED;
+                                cycles_door_unlocked_left <= cycles_door_open;
+                                door_open                 <= '0';
+                            else
+                                cycles_door_unlocked_left <= cycles_door_unlocked_left - 1;
+                            end if;
+                        end if;
+
+                    when DOOR_PERMANENTLY_LOCKED =>
+                end case;
+            end if;
+        end if;
+
+    end process p_beh;
+
+end architecture rtl;
---
+++ practice_exercises/lock/lock_tb.vhd
@@ -0,0 +1,203 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity lock_tb is
+end entity lock_tb;
+
+architecture rtl of lock_tb is
+
+    -- Constants
+    constant clk_period        : time    := 6 fs;
+    constant combination_width : integer := 8;
+
+    -- Signals
+    signal rst                   : std_logic                                        := '0';
+    signal clk                   : std_logic                                        := '0';
+    signal edit_lock_combination : std_logic                                        := '0';
+    signal check_combination     : std_logic                                        := '0';
+    signal input_combination     : std_logic_vector(combination_width - 1 downto 0) := (others => '0');
+    signal door_open             : std_logic;
+
+begin
+    dut : entity work.lock
+        port map
+        (
+            rst                   => rst,
+            clk                   => clk,
+            edit_lock_combination => edit_lock_combination,
+            check_combination     => check_combination,
+            input_combination     => input_combination,
+            door_open             => door_open
+        );
+
+    -- Clock process.
+    p_clk : process
+    begin
+        for i in 1 to 3000 loop
+            wait for clk_period / 2;
+            clk <= not clk;
+        end loop;
+        wait;
+    end process p_clk;
+
+    -- Stimulus process
+    stim_proc : process
+    begin
+        -- Test 01
+        wait until rising_edge(clk);
+        wait for 1 fs;
+        rst <= '1';
+        wait until rising_edge(clk);
+        wait for 1 fs;
+        rst <= '0';
+        wait until rising_edge(clk);
+        wait for 1 fs;
+        edit_lock_combination <= '1';
+        input_combination     <= "10101101";
+        wait until rising_edge(clk);
+        wait for 1 fs;
+        wait until rising_edge(clk);
+        wait for 1 fs;
+        edit_lock_combination <= '0';
+        input_combination     <= "01110010";
+        for i in 1 to 10 loop
+            wait until rising_edge(clk);
+            wait for 1 fs;
+            check_combination <= '1';
+            wait until rising_edge(clk);
+            wait for 1 fs;
+
+            assert door_open = '0'
+            report "Expected door_open = 0, got " & to_string(door_open)
+                severity error;
+        end loop;
+
+        wait until rising_edge(clk);
+        wait for 1 fs;
+        input_combination <= "10101101";
+        wait until rising_edge(clk);
+        wait for 1 fs;
+
+        assert door_open = '0'
+        report "Expected door_open = 0, got " & to_string(door_open)
+            severity error;
+        check_combination <= '0';
+
+        -- Test 02
+        wait until rising_edge(clk);
+        wait for 1 fs;
+        rst <= '1';
+        wait until rising_edge(clk);
+        wait for 1 fs;
+        rst                   <= '0';
+        edit_lock_combination <= '1';
+        wait until rising_edge(clk);
+        wait until rising_edge(clk);
+        wait until rising_edge(clk);
+        input_combination <= "11110000";
+
+        assert door_open = '1'
+        report "Expected door_open = 1, got " & to_string(door_open)
+            severity error;
+        wait until rising_edge(clk);
+        wait until rising_edge(clk);
+        wait until rising_edge(clk);
+        input_combination <= "00001010";
+
+        assert door_open = '1'
+        report "Expected door_open = 1, got " & to_string(door_open)
+            severity error;
+        wait until rising_edge(clk);
+        wait until rising_edge(clk);
+        wait for 1 fs;
+        edit_lock_combination <= '0';
+        wait until rising_edge(clk);
+        wait until rising_edge(clk);
+        wait until rising_edge(clk);
+        wait until rising_edge(clk);
+        wait until rising_edge(clk);
+        wait for 1 fs;
+        input_combination <= (others => '0');
+        check_combination <= '1';
+        wait until rising_edge(clk);
+        wait for 1 fs;
+        assert door_open = '0'
+        report "Expected door_open = 0, got " & to_string(door_open)
+            severity error;
+
+        input_combination <= "01101001";
+        wait until rising_edge(clk);
+        wait for 1 fs;
+        assert door_open = '0'
+        report "Expected door_open = 0, got " & to_string(door_open)
+            severity error;
+
+        input_combination <= "00001010";
+        wait until rising_edge(clk);
+        wait for 1 fs;
+        assert door_open = '1'
+        report "Expected door_open = 1, got " & to_string(door_open)
+            severity error;
+
+        check_combination <= '0';
+        for i in 1 to 10 loop
+            wait until rising_edge(clk);
+        end loop;
+        wait for 1 fs;
+        assert door_open = '0'
+        report "Expected door_open = 0, got " & to_string(door_open)
+            severity error;
+        check_combination <= '1';
+
+        -- Test 03
+        wait until rising_edge(clk);
+        wait for 1 fs;
+        assert door_open = '1'
+        report "Expected door_open = 1, got " & to_string(door_open)
+            severity error;
+
+        edit_lock_combination <= '1';
+        wait until rising_edge(clk);
+        wait for 1 fs;
+        input_combination <= "11010010";
+        wait until rising_edge(clk);
+        wait for 1 fs;
+        edit_lock_combination <= '0';
+        check_combination     <= '0';
+        assert door_open = '1'
+        report "Expected door_open = 1, got " & to_string(door_open)
+            severity error;
+        wait until rising_edge(clk);
+        wait for 1 fs;
+        assert door_open = '0'
+        report "Expected door_open = 0, got " & to_string(door_open)
+            severity error;
+
+        for i in 1 to 10 loop
+            wait until rising_edge(clk);
+        end loop;
+
+        wait for 1 fs;
+        input_combination <= "00000000";
+        check_combination <= '1';
+        wait until rising_edge(clk);
+        wait for 1 fs;
+
+        assert door_open = '0'
+        report "Expected door_open = 0, got " & to_string(door_open)
+            severity error;
+
+        input_combination <= "11010010";
+        wait until rising_edge(clk);
+        wait for 1 fs;
+
+        assert door_open = '1'
+        report "Expected door_open = 1, got " & to_string(door_open)
+            severity error;
+        check_combination <= '0';
+
+        wait;
+    end process stim_proc;
+
+end architecture rtl;
